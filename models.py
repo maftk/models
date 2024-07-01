77613f6f971f968de4c8421fa4a55799133672f1
root <root@992492b356a6>
2024-07-01 07:08:43 +0000

    mod layers > num_layers

--- models.py
+++ models.py
@@ -1,26 +1,27 @@
 from torch import nn
 import torch
 class custom1(nn.Module):
-    def __init__(self, input_size, hidden_size, num_layers):
+    def __init__(self, isize, hsize, layers, batchs):
         super().__init__()
-        self.input_size = input_size
-        self.hidden_size = hidden_size
-        self.num_layers = num_layers
-        self.lstm = nn.LSTM(input_size=self.input_size,
-                            hidden_size=self.hidden_size,
-                            num_layers=self.num_layers,
+        self.isize = isize
+        self.hsize = hsize
+        self.layers = layers
+        self.batchs = batchs
+        self.lstm = nn.LSTM(input_size=self.isize,
+                            hidden_size=self.hsize,
+                            num_layers=self.layers,
                             batch_first=True,
                             )
-        self.line = nn.Linear(hidden_size, 1)
+        self.dense = nn.Linear(hsize, 1)
         # self.drop = nn.Dropout(0.2)
         # self.sm = nn.Softmax(dim=0)
 
-    def forward(self, x, bch,hls):
-        h0 = torch.zeros(self.num_layers, bch,hls)
-        c0 = torch.zeros(self.num_layers, bch,hls)
+    def forward(self, x, batch,hsize):
+        h0 = torch.zeros(self.num_layers, batch, hsize)
+        c0 = torch.zeros(self.num_layers, batch, hsize)
         #隠れ層、cellの内部状態は使用しない。
         lstmO, (hn, cn) = self.lstm(x, (h0,c0))
-        lineO = self.line(lstmO[:,-1,:].view(x.size(0), -1))
+        lineO = self.dense(lstmO[:,-1,:].view(x.size(0), -1))
         # return torch.sigmoid(linear_out)
         # StandardScalerを使うためsigmoidで０１にしない
         return lineO
